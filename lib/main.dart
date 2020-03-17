26831763bda41923fb76f1acfd637ec6e42308e6
Corey Carter <corey-carter@hotmail.com>
2020-03-17 11:15:22 -0600

    Updated images

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'pages/home_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  
+
   runApp(FunnyBunny());
 }
 
